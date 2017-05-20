31481edfb265c080fcfc6bb2888f74ba7e870b10
Daniel Słaby <pufek93@gmail.com>
2017-05-20 16:07:11 +0200

    updated service worker

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,1 +1,30 @@
-   console.log('Am In babe :D!');
+const CACHE_NAME = 'talkie-cache';
+const cache_urls = [
+    './bundle.js'
+];
+
+self.addEventListener('install', function (event) {
+    console.log('[SW] Install', event);
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(cache_urls);
+            })
+    );
+});
+
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
